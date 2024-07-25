9462c7ce705f1ba2cd035a49584937e71f188ff4
Nishant Molleti <75113251+NISH-Original@users.noreply.github.com>
2024-07-25 07:10:08 -0700

    refactoring and organisation

--- openglpractice/main.cpp
+++ openglpractice/main.cpp
@@ -2,19 +2,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
+#include "shaderClass.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
 
 // process all input on window
 void processInput(GLFWwindow* window)
@@ -72,79 +63,23 @@ int main()
 	// here the viewport goes from (0, 0) to (800, 800)
 	glViewport(0, 0, 800, 800);
 
-	// create vertex shader object and get reference
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	// attach vertex shader source to vertex shader object
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	// compile shader into machine code
-	glCompileShader(vertexShader);
+	// creates shader object using shaders default.vert and default.frag
+	Shader shaderProgram("default.vert", "default.frag");
 
-	// check if vertexShader has successfully compiled
-	int successVertex;
-	char infoLogVertex[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &successVertex);
+	VAO VAO1;
+	VAO1.Bind();
 
-	if (!successVertex)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLogVertex);
-		std::cout << "ERROR" << infoLogVertex << std::endl;
-	}
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO VBO1(vertices, sizeof(vertices));
+	// Generates Element Buffer Object and links it to vertices
+	EBO EBO1(indices, sizeof(indices));
 
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	// check if fragmentShader has successfully compiled
-	int successFragment;
-	char infoLogFragment[512];
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &successFragment);
-
-	if (!successFragment)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLogFragment);
-		std::cout << "ERROR" << infoLogFragment << std::endl;
-	}
-
-	// create shader program object and get reference
-	GLuint shaderProgram = glCreateProgram();
-	// attach vertex and fragment shaders to shader program
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	// wrap up/link all shaders together into the shader program
-	glLinkProgram(shaderProgram);
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	// create reference containers for 
-	// vertex array and vertex buffer objects
-	GLuint VAO, VBO, EBO;
-
-	// ALWAYS generate VAOs before VBOs !!!
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-	// make VAO the current vertex array object by binding it
-	glBindVertexArray(VAO);
-
-	// bind the VBO specifying its a GL_ARRAY_BUFFER
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// introduce the vertices into the VBO
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// configure the vertex attribute so opengl knows how to read the vbo
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	// bind both VBO and VAO to 0 so we don't modify it by mistake
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
+	// Links VBO to VAO
+	VAO1.LinkVBO(VBO1, 0);
+	// Unbind to prevent accidentally modifying the objects
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -154,8 +89,8 @@ int main()
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// clear the back buffer and assign new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
+		shaderProgram.Activate();
+		VAO1.Bind();
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		// swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
@@ -164,10 +99,10 @@ int main()
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-	glDeleteProgram(shaderProgram);
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	shaderProgram.Delete();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
